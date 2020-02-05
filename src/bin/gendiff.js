3a4e7b33811706cfa34dfc9cdf954a081fb823fa
Victor Zhuravlev <mocambe@mail.ru>
2020-02-05 11:13:09 +0300

    complete step 2. add options and format

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -4,6 +4,12 @@ import program from 'commander';
 
 program
   .version('1.0.0')
+  .option('-f, --format [type]', 'output format')
   .description('Compares two configuration files and shows a difference')
+  .arguments('<firstConfig> <secondConfig>')
+  .action((firstConfig, secondConfig) => {
+    firstConfigValue = firstConfig;
+    secondConfigValue = secondConfig;
+  })
 
   program.parse(process.argv);
