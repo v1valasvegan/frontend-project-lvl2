b8462e925766df2e8eabfa078c98d9fd071b51c2
Victor Zhuravlev <mocambe@mail.ru>
2020-02-06 18:41:05 +0300

    new test and fixtures for yaml

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -2,12 +2,18 @@ import path from 'path';
 import fs from 'fs';
 import genDiff from '../dist';
 
+const formats = ['json', 'yaml'];
+let expected;
 
 const getFixturePath = (name) => path.join(__dirname, '..', '__fixtures__', name);
 
-test('compare jsons', () => {
-  const expected = fs.readFileSync(getFixturePath('result'), 'utf-8');
-  const before = getFixturePath('before.json');
-  const after = getFixturePath('after.json');
+beforeAll(() => {
+  expected = fs.readFileSync(getFixturePath('result'), 'utf-8');
+});
+
+
+test.each(formats)('%p', (format) => {
+  const before = getFixturePath(`before.${format}`);
+  const after = getFixturePath(`after.${format}`);
   expect(genDiff(before, after)).toEqual(expected);
 });
