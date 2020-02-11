dea3e3e3fb991353b18f9ff61773ecc79a07edb5
Victor Zhuravlev <mocambe@mail.ru>
2020-02-11 13:03:54 +0300

    refactor tests, add test for plain format

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -2,18 +2,23 @@ import path from 'path';
 import fs from 'fs';
 import genDiff from '../dist';
 
-const formats = ['json', 'yaml', 'ini'];
-let expected;
-
 const getFixturePath = (name) => path.join(__dirname, '..', '__fixtures__', name);
 
-beforeAll(() => {
-  expected = fs.readFileSync(getFixturePath('result'), 'utf-8');
-});
+const formats = ['json', 'yaml', 'ini'];
+const configs = formats.map((format) => [getFixturePath(`before.${format}`), getFixturePath(`after.${format}`)]);
+const expected = fs.readFileSync(getFixturePath('result.txt'), 'utf-8');
+const expectedPlain = fs.readFileSync(getFixturePath('plainResult.txt'), 'utf-8');
 
+describe('gendiff', () => {
+  test.each(configs)('%p no format provided', (before, after) => {
+    expect(genDiff(before, after)).toEqual(expectedPlain);
+  });
 
-test.each(formats)('%p', (format) => {
-  const before = getFixturePath(`before.${format}`);
-  const after = getFixturePath(`after.${format}`);
-  expect(genDiff(before, after)).toEqual(expected);
+  test.each(configs)('%p complex', (before, after) => {
+    expect(genDiff(before, after, 'complex')).toEqual(expected);
+  });
+
+  test.each(configs)('%p plain', (before, after) => {
+    expect(genDiff(before, after, 'plain')).toEqual(expectedPlain);
+  });
 });
