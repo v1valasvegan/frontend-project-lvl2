733bbe506b5094446eae670cd887ec949b45366f
Victor Zhuravlev <mocambe@mail.ru>
2020-02-16 16:59:56 +0300

    correct typos

--- __tests__/buildDiff.test.js
+++ __tests__/buildDiff.test.js
@@ -8,7 +8,7 @@ const getFixturePath = (name) => path.join(__dirname, '..', '__fixtures__', name
 const trim = (str) => {
   const strings = str.split('\n').map((string) => string.trim());
   return strings.join('');
-}
+};
 
 const processFixture = (fixture) => {
   const format = path.extname(fixture);
@@ -19,7 +19,6 @@ const processFixture = (fixture) => {
 const formats = ['yaml', 'ini', 'json'];
 const configs = formats.map((format) => [processFixture(`before.${format}`), processFixture(`after.${format}`)]);
 const expectedRaw = fs.readFileSync(getFixturePath('diff.txt'), 'utf-8');
-// const expected = expectedRaw.replace(/ /gi, '').replace(/\n/gi, '');
 
 test.each(configs)('%p', (config1, config2) => {
   expect(JSON.stringify(buildDiff(config1, config2))).toEqual(trim(expectedRaw));
