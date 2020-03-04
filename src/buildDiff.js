9f713a4ee5932d1ac7f854b2fd867b26ff9ce052
Victor Zhuravlev <mocambe@mail.ru>
2020-03-04 12:02:06 +0300

    refactor buildDiff

--- src/buildDiff.js
+++ src/buildDiff.js
@@ -1,27 +1,29 @@
 import _ from 'lodash';
 
-export default (config1, config2) => {
+const buildDiff = (config1, config2) => {
   const iter = (coll1, coll2, key) => {
     const value1 = coll1[key];
     const value2 = coll2[key];
-    if (_.isEqual(value1, value2)) {
-      return [key, 'unchanged', value1];
-    }
-
-    const areBothObjects = (val2, val1) => _.isPlainObject(val1) && _.isPlainObject(val2);
 
     if (_.has(coll1, key) && _.has(coll2, key)) {
+      if (_.isEqual(value1, value2)) {
+        return [key, 'unchanged', value1];
+      }
+
+      const areBothObjects = (val2, val1) => _.isPlainObject(val1) && _.isPlainObject(val2);
+
       if (!areBothObjects(value1, value2)) {
         return [key, 'changed', value1, value2];
       }
-      const keys = _.union(_.keys(value1), _.keys(value2)).sort();
-      const children = keys.reduce((acc, curr) => [...acc, iter(value1, value2, curr)], []);
-      return [key, 'unchanged', children];
+
+      return [key, 'unchanged', buildDiff(value1, value2)];
     }
 
     return _.has(coll1, key) ? [key, 'deleted', value1, null] : [key, 'added', null, value2];
   };
 
-  const keys = _.union(_.keys(config1), _.keys(config2));
-  return keys.reduce((acc, key) => [...acc, iter(config1, config2, key)], []);
+  const keys = _.union(_.keys(config1), _.keys(config2)).sort();
+  return keys.map((key) => iter(config1, config2, key));
 };
+
+export default buildDiff;
