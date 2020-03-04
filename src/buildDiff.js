670683e1a789a84ac1789f8db260525c88c6cdf6
Victor Zhuravlev <mocambe@mail.ru>
2020-03-04 12:36:21 +0300

    refactor buildDiff. change node presentation from array to object

--- src/buildDiff.js
+++ src/buildDiff.js
@@ -7,23 +7,33 @@ const buildDiff = (config1, config2) => {
 
     if (_.has(coll1, key) && _.has(coll2, key)) {
       if (_.isEqual(value1, value2)) {
-        return [key, 'unchanged', value1];
+        return { key, type: 'unchanged', value1 };
       }
 
       const areBothObjects = (val2, val1) => _.isPlainObject(val1) && _.isPlainObject(val2);
 
       if (!areBothObjects(value1, value2)) {
-        return [key, 'changed', value1, value2];
+        return {
+          key, type: 'changed', value1, value2,
+        };
       }
 
-      return [key, 'unchanged', buildDiff(value1, value2)];
+      return { key, type: 'unchanged', value: buildDiff(value1, value2) };
     }
 
-    return _.has(coll1, key) ? [key, 'deleted', value1, null] : [key, 'added', null, value2];
+    return _.has(coll1, key)
+      ? {
+        key, type: 'deleted', value1, value2: null,
+      }
+      : {
+        key, type: 'added', value1: null, value2,
+      };
   };
 
   const keys = _.union(_.keys(config1), _.keys(config2)).sort();
-  return keys.map((key) => iter(config1, config2, key));
+  const result = keys.map((key) => iter(config1, config2, key));
+  console.log(result);
+  return result;
 };
 
 export default buildDiff;
