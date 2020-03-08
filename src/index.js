5624fa19bdd5986e5ae5da49daaee2af840caacf
Victor Zhuravlev <mocambe@mail.ru>
2020-03-08 09:53:18 +0300

    refactor: remove needless buildFilePath function

--- src/index.js
+++ src/index.js
@@ -4,11 +4,9 @@ import parsers from './parsers';
 import buildDiff from './buildDiff';
 import formatter from './formatters';
 
-const buildFilePath = (filename) => path.resolve(filename);
-
 export default (config1, config2, format) => {
-  const data1 = fs.readFileSync(buildFilePath(config1), 'utf-8');
-  const data2 = fs.readFileSync(buildFilePath(config2), 'utf-8');
+  const data1 = fs.readFileSync(path.resolve(config1), 'utf-8');
+  const data2 = fs.readFileSync(path.resolve(config2), 'utf-8');
   const format1 = path.extname(config1).slice(1);
   const format2 = path.extname(config2).slice(1);
   const parsed1 = parsers(format1)(data1, 'utf-8');
