81f6dceb349c273fe776087e0889242b17dd45a7
Victor Zhuravlev <mocambe@mail.ru>
2020-03-08 09:58:30 +0300

    rename parsers function

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import fs from 'fs';
 import path from 'path';
-import parsers from './parsers';
+import selectParser from './parsers';
 import buildDiff from './buildDiff';
 import selectFormatter from './formatters';
 
@@ -9,8 +9,8 @@ export default (config1, config2, format) => {
   const data2 = fs.readFileSync(path.resolve(config2), 'utf-8');
   const format1 = path.extname(config1).slice(1);
   const format2 = path.extname(config2).slice(1);
-  const parsed1 = parsers(format1)(data1, 'utf-8');
-  const parsed2 = parsers(format2)(data2, 'utf-8');
+  const parsed1 = selectParser(format1)(data1, 'utf-8');
+  const parsed2 = selectParser(format2)(data2, 'utf-8');
   const diff = buildDiff(parsed1, parsed2);
   return selectFormatter(format)(diff);
 };
