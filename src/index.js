8f1b9922c38ab355282387504bd74960aeea14a8
Victor Zhuravlev <mocambe@mail.ru>
2020-03-08 09:56:21 +0300

    rename formatter function

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import fs from 'fs';
 import path from 'path';
 import parsers from './parsers';
 import buildDiff from './buildDiff';
-import formatter from './formatters';
+import selectFormatter from './formatters';
 
 export default (config1, config2, format) => {
   const data1 = fs.readFileSync(path.resolve(config1), 'utf-8');
@@ -12,5 +12,5 @@ export default (config1, config2, format) => {
   const parsed1 = parsers(format1)(data1, 'utf-8');
   const parsed2 = parsers(format2)(data2, 'utf-8');
   const diff = buildDiff(parsed1, parsed2);
-  return formatter(format)(diff);
+  return selectFormatter(format)(diff);
 };
