0586c44a345d95fde67a117d57d55ecfda589746
Victor Zhuravlev <mocambe@mail.ru>
2020-03-16 01:59:59 +0300

    refactor ini parser, change ini fixtures

--- src/parsers.js
+++ src/parsers.js
@@ -1,10 +1,23 @@
 import yaml from 'js-yaml';
 import ini from 'ini';
+import * as _ from 'lodash';
+
+const processIni = (iniObj) => {
+  const iter = (value) => {
+    if (!_.isPlainObject(value)) {
+      return parseInt(value, 10) || value;
+    }
+    return processIni(value);
+  };
+  const parsedEntries = _.entries(iniObj).map(([key, value]) => [key, iter(value)]);
+  return Object.fromEntries(parsedEntries);
+};
+
 
 const mapFormatToParser = {
   json: JSON.parse,
   yaml: yaml.safeLoad,
-  ini: ini.parse,
+  ini: (config) => processIni(ini.parse(config)),
 };
 
 export default (format) => mapFormatToParser[format];
