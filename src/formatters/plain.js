9e104a1ea14f3a2c9755079b8d8896ad5fb97d93
Victor Zhuravlev <mocambe@mail.ru>
2020-03-22 13:23:57 +0300

    refactor plain formatter

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -1,43 +1,32 @@
 import _ from 'lodash';
 
-const stringify = (value, changed = false) => {
-  if (_.isString(value)) {
-    return changed ? `'${value}'` : `value: '${value}'`;
-  }
-  if (_.isObject(value)) {
-    return '[complex value]';
-  }
-  return changed ? `${value}` : `value: ${value}`;
+const stringify = (value) => {
+  if (_.isPlainObject(value)) return '[complex value]';
+  if (_.isString(value)) return `'${value}'`;
+  return value;
 };
 
-const enumTemplates = {
-  deleted: () => 'deleted',
-  added: (_v1, v2) => `added with ${stringify(v2)}`,
-  changed: (v1, v2) => `changed from ${stringify(v1, true)} to ${stringify(v2, true)}`,
-  nested: (v1, v2) => `changed from ${stringify(v1, true)} to ${stringify(v2, true)}`,
-  unchanged: () => null,
+const process = (tree, pathAcc) => {
+  const iter = (data, acc) => {
+    const {
+      name, type, value1, value2, children,
+    } = data;
+    const newAcc = `${acc}.${name}`;
+    const path = `'${newAcc.slice(1)}'`;
+    const valuePrefix = (v) => (_.isPlainObject(v) ? '' : 'value: ');
+
+    switch (type) {
+      case 'unchanged': return null;
+      case 'changed': return `Property ${path} was changed from ${stringify(value1, true)} to ${stringify(value2, true)}`;
+      case 'nested': return process(children, newAcc);
+      case 'deleted': return `Property ${path} was deleted`;
+      case 'added': return `Property ${path} was added with ${valuePrefix(value2)}${stringify(value2)}`;
+      default: throw new Error(`unknow type ${type}`);
+    }
+  };
+
+  const processedNodes = tree.flatMap((node) => (iter(node, pathAcc)));
+  return _.compact(processedNodes).join('\n');
 };
 
-const buildTemplateData = (data, acc) => {
-  const {
-    name, type, value1, value2, children,
-  } = data;
-  const newAcc = `${acc}.${name}`;
-
-  switch (type) {
-    case 'unchanged': return null;
-    case 'nested': return children.flatMap((item) => buildTemplateData(item, newAcc));
-    case 'added': return { data: { type, value1, value2 }, path: newAcc.slice(1) };
-    case 'deleted': return { data: { type, value1, value2 }, path: newAcc.slice(1) };
-    case 'changed': return { data: { type, value1, value2 }, path: newAcc.slice(1) };
-    default: throw new Error(`Unknown type ${type}`);
-  }
-};
-
-export default (diff) => {
-  const processedData = diff.flatMap((item) => buildTemplateData(item, ''));
-  const templateData = _.compact(processedData);
-  return templateData.map(({ data: { type, value1, value2 }, path }) => (
-    `Property '${path}' was ${enumTemplates[type](value1, value2)}`
-  )).join('\n');
-};
+export default (diff) => process(diff, '');
